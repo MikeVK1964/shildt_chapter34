ee953040b1d203e826632bd66786a9aa1be516b6
MikeVK <mikekondaurov2008@gmail.com>
2024-03-18 17:02:32 +0300

    sort для vector

--- main.cpp
+++ main.cpp
@@ -3,17 +3,26 @@
 #include <QDebug>
 
 using namespace std;
+void PrintArray(vector<int> v1)
+{
+    for (int i=0;i<v1.size();i++) qDebug()<< v1[i];
+
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     vector<int> v1 = {1,2,10,-15 };
-    for (auto i=0;i<3;i++) qDebug()<< v1[i];
+ //   for (int i=0;i<v1.size();i++) qDebug()<< v1[i];
+    PrintArray(v1);
     qDebug() << "----------------";
     ////vector<int> *p;
 //    auto p=v1.begin();
     auto ss=adjacent_find(v1.begin(),v1.end());
     auto ss1=min_element(v1.begin(),v1.end());
     qDebug()<<*ss1;
+    sort(v1.begin(),v1.end());
+    PrintArray(v1);
+
     return a.exec();
 }
