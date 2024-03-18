ecfa224e25b0e4cbc4c780386f4fe7b658f178ff
MikeVK <mikekondaurov2008@gmail.com>
2024-03-18 04:37:56 +0300

    Функция min_element

--- main.cpp
+++ main.cpp
@@ -7,12 +7,13 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    vector<int> v1 = {1,2,2 };
+    vector<int> v1 = {1,2,10,-15 };
     for (auto i=0;i<3;i++) qDebug()<< v1[i];
     qDebug() << "----------------";
     ////vector<int> *p;
     auto p=v1.begin();
     auto ss=adjacent_find(v1.begin(),v1.end());
-    qDebug()<<*ss;
+    auto ss1=min_element(v1.begin(),v1.end());
+    qDebug()<<*ss1;
     return a.exec();
 }
